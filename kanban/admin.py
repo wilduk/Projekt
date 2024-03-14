9beb29014fb8315438c6fd4062d04a93603d5a34
wilduk <luckiewiczlukasz@gmail.com>
2024-03-14 12:35:18 +0100

    testy

--- kanban/admin.py
+++ kanban/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Column
+from .models import Column, Note
 
 
 @admin.register(Column)
 class ColumnAdmin(admin.ModelAdmin):
     list_display = ['name', 'position', 'max']
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'column']
